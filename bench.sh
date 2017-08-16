44971471f0d2e5aabe5848093233328b8fca804c
Rahul Nair <rahulunair@gmail.com>
2017-08-16 15:37:55 -0700

    Initail readings and posprocessing script

--- bench.sh
+++ bench.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 save_to="./log/perf.out"
-
-for i in 1 in {1..3}:
+final_out="./log/perf-out.csv"
+for i in 1 in {1..50}:
 do
 	./perf_bash.sh | tail -n +5 >> $save_to
 done
-cat $save_to | awk '!seen[$0]++' > ./log/final.out
+cat $save_to | awk '!seen[$0]++' >  $final_out
+rm $save_to
--- perf_bash.sh
+++ perf_bash.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 temp_file=/tmp/${RANDOM}
 mktemp "$temp_file.XXXXX"
-perf_command="/usr/bin/perf stat  -x","  -a -e task-clock,migrations,page-faults,cycles,instructions,branches,branch-misses,L1-dcache-loads,L1-dcache-misses,"cpu/cache-misses/",LLC-loads,LLC-misses,cpu/cache-references/,power/energy-cores/,stalled-cycles-frontend,stalled-cycles-backend --log-fd 3 stress -c 4 -t 2s 3> ${temp_file}"
+perf_command="/usr/bin/perf stat  -x","  -a -e task-clock,migrations,page-faults,cycles,instructions,branches,branch-misses,L1-dcache-loads,L1-dcache-misses,"cpu/cache-misses/",LLC-loads,LLC-misses,cpu/cache-references/,power/energy-cores/,stalled-cycles-frontend,stalled-cycles-backend --log-fd 3 stress -c 4 -t 6s 3> ${temp_file}"
 echo "Executing::" $perf_command
 eval $perf_command
 # format="cat $temp_file | sed '2d' |head  -n -2  | awk 'BEGIN {FS=\",\"} {print $3\",\"$1}' |csvtool transpose -"
