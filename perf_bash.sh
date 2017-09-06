b182072f18a6ee58ad99d27b6e9380cb2ba612b4
Rahul Nair <rahulunair@gmail.com>
2017-09-06 13:57:03 -0700

    analysis for 60 % workload added

--- perf_bash.sh
+++ perf_bash.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 temp_file=/tmp/${RANDOM}
 mktemp "$temp_file.XXXXX"
-perf_command="/usr/bin/perf stat  -x","  -a -e task-clock,migrations,page-faults,cycles,instructions,branches,branch-misses,L1-dcache-loads,L1-dcache-misses,"cpu/cache-misses/",LLC-loads,LLC-misses,cpu/cache-references/,power/energy-cores/,stalled-cycles-frontend,stalled-cycles-backend --log-fd 9 stress-ng -l 30  --cpu-load-slice 0 -c 4 -t 10s 9> ${temp_file}"
+perf_command="/usr/bin/perf stat  -x","  -a -e task-clock,migrations,page-faults,cycles,instructions,branches,branch-misses,L1-dcache-loads,L1-dcache-misses,"cpu/cache-misses/",LLC-loads,LLC-misses,cpu/cache-references/,power/energy-cores/,stalled-cycles-frontend,stalled-cycles-backend --log-fd 9 stress-ng -l 60  --cpu-load-slice 0 -c 4 -t 10s 9> ${temp_file}"
 #perf_command="/usr/bin/perf stat  -x","  -a -e task-clock,migrations,page-faults,cycles,instructions,branches,branch-misses,L1-dcache-loads,L1-dcache-misses,"cpu/cache-misses/",LLC-loads,LLC-misses,cpu/cache-references/,power/energy-cores/,stalled-cycles-frontend,stalled-cycles-backend --log-fd 3 stress -c 4 -t 6s 3> ${temp_file}"
 echo "Executing::" $perf_command
 eval $perf_command
